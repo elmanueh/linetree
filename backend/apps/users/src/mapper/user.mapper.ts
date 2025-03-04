599fed11d2a1a3469c5b755ba07d24a88c1794fd
Manuel Bernabé Rodríguez <bernabe2003@gmail.com>
2025-03-04 13:22:50 +0100

    feat: new dtos

--- backend/apps/users/src/mapper/user.mapper.ts
+++ backend/apps/users/src/mapper/user.mapper.ts
@@ -1,9 +1,10 @@
-import { CreateUserDto } from '../dto/create-user.dto';
 import { GetUserDto } from '../dto/get-user.dto';
+import { LoginUserDto } from '../dto/login-user.dto';
+import { CreateUserDto } from '../dto/register-user.dto';
 import { User } from '../schema/user.schema';
 
 export class UserMapper {
-  static toDomain(dto: CreateUserDto): User {
+  static toDomainRegister(dto: CreateUserDto): User {
     const user = new User();
     user.name = dto.name;
     user.email = dto.email;
@@ -11,9 +12,15 @@ export class UserMapper {
     return user;
   }
 
+  static toDomainLogin(dto: LoginUserDto) {
+    const user = new User();
+    user.email = dto.email;
+    user.password = dto.password;
+    return user;
+  }
+
   static toDto(user: User): GetUserDto {
     const dto = new GetUserDto();
-    dto.id = user.id;
     dto.name = user.name;
     dto.email = user.email;
     dto.password = user.password;

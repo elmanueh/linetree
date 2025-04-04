5891abb64be6ea8e8562d5badfd8080f7feb8004
Manuel Bernabé Rodríguez <bernabe2003@gmail.com>
2025-04-04 20:58:43 +0200

    fix: correct exception type handling in JsonToHttpExceptionFilter

--- backend/apps/app/src/exceptions/json-http.filter.ts
+++ backend/apps/app/src/exceptions/json-http.filter.ts
@@ -8,13 +8,15 @@ import { Response } from 'express';
 
 @Catch()
 export class JsonToHttpExceptionFilter implements ExceptionFilter {
-  catch(exception: { message: string; status: number }, host: ArgumentsHost) {
+  catch(exception: { status: string; message: string }, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
     const message = exception.message;
-    const status = exception.status;
-    const error = HttpStatus[status];
+    const error = exception.status;
+    const status =
+      HttpStatus[error as keyof typeof HttpStatus] |
+      HttpStatus.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({ status, error, message });
   }

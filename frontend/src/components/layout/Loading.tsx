7691ba3478fe0fd26e73fa8697936f3d851bee99
Manuel Bernabé Rodríguez <bernabe2003@gmail.com>
2025-06-13 21:02:27 +0200

    fix: remove unnecessary height and background from Loading component

--- frontend/src/components/layout/Loading.tsx
+++ frontend/src/components/layout/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen bg-white">
+    <div className="flex items-center justify-center">
       <div className="w-16 h-16 border-4 border-green-700 border-dashed rounded-full animate-spin"></div>
     </div>
   )

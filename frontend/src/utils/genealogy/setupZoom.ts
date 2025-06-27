862e3d96ff2c8df77846cbaa22d4d15e93ba925c
Manuel Bernabé Rodríguez <42030244+elmanueh@users.noreply.github.com>
2025-06-28 01:49:12 +0200

    refactor(genealogy): enhance setupZoom function by adding scaleExtent for zoom limits

--- frontend/src/utils/genealogy/setupZoom.ts
+++ frontend/src/utils/genealogy/setupZoom.ts
@@ -4,9 +4,12 @@ export function setupZoom(
   svg: d3.Selection<SVGSVGElement, unknown, null, undefined>,
   g: d3.Selection<SVGGElement, unknown, null, undefined>
 ) {
-  const zoom = d3.zoom<SVGSVGElement, unknown>().on('zoom', (event) => {
-    g.attr('transform', event.transform)
-  })
+  const zoom = d3
+    .zoom<SVGSVGElement, unknown>()
+    .scaleExtent([0.5, 2])
+    .on('zoom', (event) => {
+      g.attr('transform', event.transform)
+    })
 
   svg.call(zoom)
   return zoom
